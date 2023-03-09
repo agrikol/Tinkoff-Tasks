e9d6b410d2ca82c80d94457efd9451e41e78ab73
agrikol <agrikol1995@yandex.ru>
2023-03-09 11:31:29 +0700

    8

--- Task3.py
+++ Task3.py
@@ -1,15 +1,15 @@
 from math import gcd  # Импорт функции НОД
 from math import prod  # Импорт функции произведения итерируемых
 
-n, s, m = int(input()), [], []  # Переменные: n, список "a + b = n", список для НОК,
-for a in range(1, n):  # Вложенный цикл для определения и внесения возможных "n = a + b"
+n, s, m = int(input()), [], []  # Переменные: n, список "a + b = n", список для НОК
+for a in range(1, n): # Вложенный цикл для определения и внесения возможных "n = a + b"
     for b in range(1, n):
         if a + b == n:
             s.append([])
             s[a - 1].append(a)
             s[a - 1].append(b)
 
-for i in range(len(s)):  # Цикл, определяющий НОК
+for i in range(len(s)): # Цикл, определяющий НОК
     m.append([])
     m[i] = prod(s[i]) / gcd(s[i][0], s[i][1])
-print(*s[m.index(min(m))])  # По индексу списка m находим нужный нам минимальный НОК в списке s
+print(*s[m.index(min(m))]) # По индексу списка m находим нужный нам минимальный НОК в списке s
---
+++ Task6.py
@@ -0,0 +1,14 @@
+s, x = [], 0 # Переменные: список, накопитель max
+for i in range(int(input('Введите количество запросов: '))):
+    a = int(input())
+    if a not in s: # Проверка на множество (class 'set' не использован из-за свойства неупорядоченности)
+        s.append(a)
+    if len(s) > 1:
+        for j in range(len(s)): # Цикл для поиска max xor
+            for h in range(1, len(s)):
+                if s[j] ^ s[h] >= x:
+                    x = s[j] ^ s[h]
+        print(x) # Вывод xor
+    else:
+        print(0)
+
